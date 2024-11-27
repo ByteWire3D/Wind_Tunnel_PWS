943bcd743e990c3617e13334bcc380b8b70876d2
ByteWire3D <165397383+ByteWire3D@users.noreply.github.com>
2024-11-27 20:52:32 +0100

    add voltatge metrics to logg

--- PWS-code/PWS_6_modules/datalogger/datalogger.ino
+++ PWS-code/PWS_6_modules/datalogger/datalogger.ino
@@ -151,7 +151,7 @@ void setup() {
   testFolderName = "/test_results/" + String(FolderName) + " (" + String(testCounter) + ") " + String(test_datum);
   createDir(SD, testFolderName.c_str());
   //file_firstline = "Looptime;Loop Number;Setpoint;Airspeed;Error;Output;Lift;Drag;Pitch;Ampere;Voltage;Wattage;Mah Used";
-  file_firstline = "Looptime;Setpoint;Lift;Drag;Pitch";
+  file_firstline = "Looptime;Setpoint;Lift;Drag;Pitch;Ampere;Voltage;Wattage;Mah Used";
   //Serial.printf("Created folder: %s\n", testFolderName.c_str());
   appendFile(SD, (testFolderName + "/test_data.txt").c_str(), file_firstline);
 
@@ -573,7 +573,7 @@ void waitForData(HardwareSerial &serial, T &data, unsigned long max_wait_time_ms
         //Serial.print("data recieved from:");
         // Serial.println(deviceName);
        // string_data = String(data.looptime) + ";" + String(data.loop_number) + ";" + String(data.setpoint) + ";" + String(data.airspeed) + ";" + String(data.error) + ";" + String(data.output) + ";" + String(data.lift_loadcell) + ";" + String(data.drag_loadcell) + ";"+ String(data.pitch) + ";" + String(data.ampere) + ";" + String(data.voltage) + ";" + String(data.wattage) + ";" + String(data.mah_used);
-        string_data = String(data.looptime) + ";" + String(data.setpoint) + ";" + String(data.lift_loadcell) + ";" + String(data.drag_loadcell) + ";"+ String(data.pitch);
+        string_data = String(data.looptime) + ";" + String(data.setpoint) + ";" + String(data.lift_loadcell) + ";" + String(data.drag_loadcell) + ";"+ String(data.pitch) + ";"+ String(data.ampere) + ";" + String(data.voltage) + ";" + String(data.wattage) + ";" + String(data.mah_used);
         appendFile(SD, (testFolderName + "/test_data.txt").c_str(), string_data);
       }
       return;
