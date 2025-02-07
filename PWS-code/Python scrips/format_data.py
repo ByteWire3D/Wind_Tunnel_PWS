ffb6d437e96de5c414c4dfb72d9dc7c574019908
ByteWire3D <165397383+ByteWire3D@users.noreply.github.com>
2025-02-07 11:40:12 +0100

    Update format_data.py
    delete unused functions

--- PWS-code/Python scrips/format_data.py
+++ PWS-code/Python scrips/format_data.py
@@ -4,10 +4,6 @@ data = """
 
 """
 
-
-# List of words or phrases to add a newline before
-keywords = ["|pid find speed", "Waiting for pid", "Testing", "keep speed"]
-
 # Replace every "Waiting for PID" and "Testing" with a newline followed by that string
 formatted_data = re.sub(r'(Waiting for PID|Testing|pid find speed|keep speed|s:1 command received|s:2 command received|s:3 command received|s:4 command received)', r'\n\1', data)
 
